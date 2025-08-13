8a499e4a18c14a23a8f286b7ac9e1ad891ad4cb3
Abby Chung <67236934+abby-chung@users.noreply.github.com>
2025-08-13 04:28:36 +0000

    Rollback nav

--- src/App.jsx
+++ src/App.jsx
@@ -71,12 +71,7 @@ function Layout({ children }) {
     <div className="max-w-3xl mx-auto">
       <header>
         <nav>
-          {/* <Link to="/batatas/">Home</Link> | <Link to="/blog">Blog</Link> | <Link to="/about">About</Link> */}
-          <ul>
-            <li><a href="/batatas/">Home</a></li>
-            <li><a href="/blog">Blogs</a></li>
-            <li><a href="/about">About</a></li>
-          </ul>
+          <Link to="/batatas/">Home</Link> | <Link to="/blog">Blog</Link> | <Link to="/about">About</Link>
         </nav>
       </header>
       <main>{children}</main>
